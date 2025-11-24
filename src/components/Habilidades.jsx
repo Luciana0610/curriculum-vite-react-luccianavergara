19469f2f5e3ce4fe6b72d83b8a483351f92d2ef6
Lucciana Vergara Martinez <lucianavergaramartinez@gmail.com>
2025-11-24 09:55:42 -0500

    feat:componente Habilidades dinamico con formación complementario

--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -1,14 +1,45 @@
-export default function Habilidades() {
-  const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Git', 'Vite'];
+import React from "react";
+
+function Habilidades() {
+  const educacion = [
+    {
+      institucion: "I.E José Antonio Galán",
+      curso: "Bachiller Académico",
+      año: 2024,
+    },
+    {
+      institucion: "Politecnico Jaime Isaza Cadavid",
+      curso: "Técnico Laboral en Sistemas Informáticos",
+      año: 2025,
+    },
+    {
+      institucion: "SENA - Centro de Tecnología de la Manufactura Avanzada (CTMA)",
+      curso: "Tecnólogo en Análisis y Desarrollo de Software (ADSO)",
+      año: "2025 - 2027",
+    },
+    {
+      institucion: "SENA",
+      curso: "Asesoria para el uso de las TIC",
+      año: 2025,
+    },
+    {
+      institucion: "SENA",
+      curso: "Inglés Nivel 1",
+      año: 2025,
+    },
+  ];
+
   return (
-    <section className='section skills'>
-      <h2>Habilidades</h2>
-      <ul>
-        {skills.map((skill) => (
-          <li key={skill}>{skill}</li>
+    <div>
+      <ul style={{ listStyleType: "square", lineHeight: "1.8" }}>
+        {educacion.map((edu, index) => (
+          <li key={index}>
+            <strong>{edu.curso}</strong> - {edu.institucion} ({edu.año})
+          </li>
         ))}
       </ul>
-    </section>
+    </div>
   );
-  
 }
+
+export default Habilidades;
