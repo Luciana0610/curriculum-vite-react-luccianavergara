0ff53ac467b170d629c954d9345cdff520aa9392
Lucciana Vergara Martinez <lucianavergaramartinez@gmail.com>
2025-11-24 07:24:10 -0500

    feat:componente Contacto con datos personales

---
+++ src/components/Contacto.jsx
@@ -0,0 +1,9 @@
+export default function Contacto() {
+  return (
+    <section className='section contact'>
+      <h2>Contacto</h2>
+      <p>¿Quieres colaborar o contactarme?</p>
+      <p><strong>lucianagmail.com</strong></p>
+    </section>
+  );
+}
