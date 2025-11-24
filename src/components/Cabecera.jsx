0ee0ef7923ec0f7cec46ff282bc723e9b2176404
Lucciana Vergara Martinez <lucianavergaramartinez@gmail.com>
2025-11-24 07:03:25 -0500

    feat: componente Cabecera con datos actualizados

---
+++ src/components/Cabecera.jsx
@@ -0,0 +1,8 @@
+export default function Cabecera() {
+  return (
+    <header className="bg-blue-600 text-white p-6 text-center">
+      <h1 className="text-3xl font-bold">Lucciana Vergara</h1>
+      <p>Desarrolladora Front-End | Estudiante de Tecnología en Sistemas, Analista</p>
+    </header>
+  );
+}
