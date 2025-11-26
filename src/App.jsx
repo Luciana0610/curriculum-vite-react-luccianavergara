82b480c49293903ace415fedbce1d45e1ddb68a2
Lucciana Vergara Martinez <lucianavergaramartinez@gmail.com>
2025-11-26 07:39:22 -0500

    feat: desestructuración de props en CabeceraCV

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,30 @@ import Contacto from './components/Contacto.jsx';
 import Proyectos from './components/Proyectos.jsx';
 import StackTecnologias from './components/StackTecnologias.jsx';
 
+import { datosPersonales, perfil, experiencias, estudios } from "./data";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div id="container">
-      <Cabecera />
-      <Perfil />
-      <Habilidades />
-      <Proyectos />
+      <>
       <Contacto />
       <StackTecnologias />
+      <Cabecera
+        nombre={datosPersonales.nombre}
+        cargo={datosPersonales.cargo}
+        ciudad={datosPersonales.ciudad}
+        contacto={datosPersonales.contacto}
+      />
+
+      <Perfil resumen={perfil.resumen} />
+
+      <Proyectos lista={experiencias} />
+
+      <Habilidades estudios={estudios} />
+    </>
+
     </div>
   );
 }
